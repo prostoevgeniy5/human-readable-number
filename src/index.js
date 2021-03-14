ae2e935e99cbb9136c5cf9615bcb02d304266a4f
Evgeniy <yevgeniykyr5@ukr.netgit config --global user.name prostoevgeniy5git config --global user.email yevgeniykyr5@ukr.net>
2021-03-14 07:16:28 +0200

    feat: use convert indent to rearrange the indentation

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 module.exports = function toReadable (number) {
-    const units = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    const secondTen = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
-    const dozens = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
-    const hundreds  = ["one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred" ]
-    
-    let temp = number.toString().split('');
-    let length = temp.length;
-    let result; 
-    if(number >= 10 && number < 20) {
-          return secondTen[number -10];
+  const units = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  const secondTen = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
+  const dozens = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
+  const hundreds  = ["one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred" ]
+  
+  let temp = number.toString().split('');
+  let length = temp.length;
+  let result; 
+  if(number >= 10 && number < 20) {
+      return secondTen[number -10];
+    }
+  result = temp.map((el, ind) => {
+    let res = +el;
+    if(length === 3 && ind === 0) {
+      return hundreds[res - 1] + " ";
+    }else if(length === 3 && ind === 1) {
+      if(dozens[res - 1] === undefined) {
+        return
       }
-    result = temp.map((el, ind) => {
-      let res = +el;
-      if(length === 3 && ind === 0) {
-          return hundreds[res - 1] + " ";
-      }else if(length === 3 && ind === 1) {
-          if(dozens[res - 1] === undefined) {
-              return
-          }
-          return dozens[res - 1] + " ";
-      }else if(length === 3 && ind === 2) {
-          if(units[res - 1] === undefined) {
-              return;
-          }
-          return units[res - 1] + " ";
-      } else if (length === 2 && ind === 0 && number >= 20) {
-          return dozens[res - 1] + " ";
-      }else if(length === 2 && ind === 1 && number >= 20) {
-          if(units[res - 1] === undefined) {
-              return
-          }
-          return units[res - 1] + " ";
-      }else if(length === 1) {
-          if(units[res - 1] === undefined) {
-              return "zero"
-          } else {
-            return units[res - 1];
-          }
-
+      return dozens[res - 1] + " ";
+    }else if(length === 3 && ind === 2) {
+      if(units[res - 1] === undefined) {
+        return;
       }
-    })
-      result = result.join('');
+      return units[res - 1] + " ";
+    } else if (length === 2 && ind === 0 && number >= 20) {
+      return dozens[res - 1] + " ";
+    }else if(length === 2 && ind === 1 && number >= 20) {
+      if(units[res - 1] === undefined) {
+        return
+      }
+      return units[res - 1] + " ";
+    }else if(length === 1) {
+      if(units[res - 1] === undefined) {
+        return "zero"
+      } else {
+      return units[res - 1];
+      }
 
-      return result;
+    }
+  })
+    result = result.join('');
+
+    return result;
 }
