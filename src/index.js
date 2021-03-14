61dfba7b1b20c18e2c73bc0fda2087b3b0af3e6b
Evgeniy <yevgeniykyr5@ukr.netgit config --global user.name prostoevgeniy5git config --global user.email yevgeniykyr5@ukr.net>
2021-03-14 11:23:02 +0200

    feat: solution for task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
 module.exports = function toReadable (number) {
-  const units = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  const secondTen = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
-  const dozens = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
-  const hundreds  = ["one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred" ]
+  var units = [" one", " two", " three", " four", " five", " six", " seven", " eight", " nine"]
+  var secondTen = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
+  var dozens = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
+  var hundreds  = ["one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred" ]
   
-  let temp = number.toString().split('');
-  let length = temp.length;
-  let result; 
+  var temp = number.toString().split('');
+  var length = temp.length;
+  var result; 
   if(number >= 10 && number < 20) {
-      return secondTen[number -10];
-    }
+    return secondTen[number -10];
+  }
   result = temp.map((el, ind) => {
-    let res = +el;
-    if(length === 3 && ind === 0) {
-      return hundreds[res - 1] + " ";
-    }else if(length === 3 && ind === 1) {
-      if(dozens[res - 1] === undefined) {
-        return
-      }
-      return dozens[res - 1] + " ";
-    }else if(length === 3 && ind === 2) {
-      if(units[res - 1] === undefined) {
-        return;
-      }
-      return units[res - 1] + " ";
-    } else if (length === 2 && ind === 0 && number >= 20) {
-      return dozens[res - 1] + " ";
-    }else if(length === 2 && ind === 1 && number >= 20) {
-      if(units[res - 1] === undefined) {
-        return
-      }
-      return units[res - 1] + " ";
-    }else if(length === 1) {
-      if(units[res - 1] === undefined) {
-        return "zero"
-      } else {
-      return units[res - 1];
-      }
-
+  var res = Number(el);
+  if(length === 3 && ind === 0) {
+    return hundreds[res - 1] + " ";
+  }else if(length === 3 && ind === 1) {
+    if(dozens[res - 1] === undefined) {
+    return
+    } else if(dozens[res - 1] === "ten") {
+      return secondTen[(number % 100) - 10]
     }
-  })
-    result = result.join('');
+    return dozens[res - 1] + " ";
+  }else if(length === 3 && ind === 2) {
+    if(units[res - 1] === undefined) {
+    return;
+    } else if(+temp[ind - 1] !== 1) {
+    return units[res - 1].trim();
+    } else {
+      return
+    }
+  } else if (length === 2 && ind === 0 && number >= 20) {
+    return dozenll
+    lls[res - 1];
+  }else if(length === 2 && ind === 1 && number >= 20) {
+    if(units[res - 1] === undefined) {
+    return
+    }
+    return units[res - 1];
+  }else if(length === 1) {
+    if(units[res - 1] === undefined) {
+    return "zero"
+    } else {
+    return (units[res - 1].trim());
+    }
 
-    return result;
+  }
+  })
+  result = result.join('').trim();
+
+  return result;
 }
